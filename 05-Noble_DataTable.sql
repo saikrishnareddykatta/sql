3d36810731c6c0c8849ed5456241a165d1a773a9
Sai Krishna Reddy Katta <saikrishnareddyk95@gmail.com>
2022-11-23 12:36:09 -0800

    added noble datatable file

---
+++ 05-Noble_DataTable.sql
@@ -0,0 +1,60 @@
+SELECT * FROM nobel;
+
+/* Running this command to see what are the columns and rows available in Noble Datatable*/
+
+/*
+
+yr	subject	winner
+1901	Chemistry	Jacobus H. van 't Hoff
+1901	Literature	Sully Prudhomme
+1901	Medicine	Emil von Behring
+1901	Peace	Frédéric Passy
+1901	Peace	Henry Dunant
+1901	Physics	Wilhelm Conrad Röntgen
+1902	Chemistry	Emil Fischer
+1902	Literature	Theodor Mommsen
+1902	Medicine	Ronald Ross
+1902	Peace	Albert Gobat
+1902	Peace	Élie Ducommun
+1902	Physics	Hendrik A. Lorentz
+1902	Physics	Pieter Zeeman
+1903	Chemistry	Svante Arrhenius
+1903	Literature	Bjørnstjerne Bjørnson
+1903	Medicine	Niels Ryberg Finsen
+1903	Peace	Randal Cremer
+1903	Physics	Henri Becquerel
+1903	Physics	Marie Curie
+1903	Physics	Pierre Curie
+1904	Chemistry	Sir William Ramsay
+1904	Literature	Frédéric Mistral
+1904	Literature	José Echegaray
+1904	Medicine	Ivan Pavlov
+1904	Peace	Institute of International Law
+1904	Physics	Lord Rayleigh
+1905	Chemistry	Adolf von Baeyer
+1905	Literature	Henryk Sienkiewicz
+1905	Medicine	Robert Koch
+1905	Peace	Bertha von Suttner
+1905	Physics	Philipp Lenard
+1906	Chemistry	Henri Moissan
+1906	Literature	Giosuè Carducci
+1906	Medicine	Camillo Golgi
+1906	Medicine	Santiago Ramón y Cajal
+1906	Peace	Theodore Roosevelt
+1906	Physics	J.J. Thomson
+1907	Chemistry	Eduard Buchner
+1907	Literature	Rudyard Kipling
+1907	Medicine	Alphonse Laveran
+1907	Peace	Ernesto Teodoro Moneta
+1907	Peace	Louis Renault
+1907	Physics	Albert A. Michelson
+1908	Chemistry	Ernest Rutherford
+1908	Literature	Rudolf Eucken
+1908	Medicine	Ilya Mechnikov
+1908	Medicine	Paul Ehrlich
+1908	Peace	Fredrik Bajer
+1908	Peace	Klas Pontus Arnoldson
+1908	Physics	Gabriel Lippmann
+Results truncated. Only the first 50 rows have been shown
+
+*/
